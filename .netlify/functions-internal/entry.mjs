8eda2d2922b6babc6bec34d6b82fd8eb4336a33c
Zach Case <zach@fulfilld.io>
2023-04-07 12:01:36 -0600

    Set-up Netlify build

---
+++ .netlify/functions-internal/entry.mjs
@@ -0,0 +1,21 @@
+import * as adapter from '@astrojs/netlify/netlify-functions.js';
+import { s as server_default, g as deserializeManifest } from './chunks/astro.7b5a729d.mjs';
+import { _ as _page0 } from './chunks/pages/all.ea32a32f.mjs';
+/* empty css                                 */
+const pageMap = new Map([["src/pages/index.astro", _page0],]);
+const renderers = [Object.assign({"name":"astro:jsx","serverEntrypoint":"astro/jsx/server.js","jsxImportSource":"astro"}, { ssr: server_default }),];
+
+const _manifest = Object.assign(deserializeManifest({"adapterName":"@astrojs/netlify/functions","routes":[{"file":"","links":["/_astro/index.39048fa7.css"],"scripts":[],"routeData":{"route":"/","type":"page","pattern":"^\\/$","segments":[],"params":[],"component":"src/pages/index.astro","pathname":"/","prerender":false,"_meta":{"trailingSlash":"ignore"}}}],"base":"/","markdown":{"drafts":false,"syntaxHighlight":"shiki","shikiConfig":{"langs":[],"theme":"github-dark","wrap":false},"remarkPlugins":[],"rehypePlugins":[],"remarkRehype":{},"gfm":true,"smartypants":true},"pageMap":null,"componentMetadata":[["/Users/cow/apps/CowDotDev/src/pages/index.astro",{"propagation":"none","containsHead":true}]],"renderers":[],"entryModules":{"\u0000@astrojs-ssr-virtual-entry":"_@astrojs-ssr-virtual-entry.mjs","astro:scripts/before-hydration.js":""},"assets":["/_astro/index.39048fa7.css","/favicon.svg"]}), {
+	pageMap: pageMap,
+	renderers: renderers
+});
+const _args = {};
+const _exports = adapter.createExports(_manifest, _args);
+const handler = _exports['handler'];
+
+const _start = 'start';
+if(_start in adapter) {
+	adapter[_start](_manifest, _args);
+}
+
+export { handler, pageMap, renderers };
